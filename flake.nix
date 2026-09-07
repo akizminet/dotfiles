417fea6cedc397731aee17b585b10a4eb6aad2b3
akizminet <akizminet@gmail.com>
2026-09-07 07:41:23 +0700

    feat(flake): declare desktop packages and addons in user profile

--- flake.nix
+++ flake.nix
@@ -15,36 +15,45 @@
         config.allowUnfree = true;
       };
       nixGLIntel = nixgl.packages.${system}.nixGLIntel;
+      fcitx5WithAddons = pkgs.kdePackages.fcitx5-with-addons.override {
+        addons = [
+          pkgs.kdePackages.fcitx5-unikey
+          pkgs.kdePackages.fcitx5-configtool
+          pkgs.fcitx5-gtk
+          pkgs.fcitx5-bamboo
+        ];
+      };
     in
     {
-      packages.${system}.default = pkgs.buildEnv {
-        name = "default-profile";
-        paths = [
-          # GUI & Desktop Applications
-          pkgs.google-chrome
-          pkgs.libreoffice
-          pkgs.flameshot
+      packages.${system} = {
+        default = pkgs.buildEnv {
+          name = "default-profile";
+          paths = [
+            # GUI & Desktop Applications
+            pkgs.google-chrome
+            pkgs.libreoffice
+            pkgs.flameshot
 
-          # CLI Tools
-          pkgs.ffmpeg
-          pkgs.gh
-          pkgs.tailscale
+            # CLI & Network Tools
+            pkgs.ffmpeg-full
+            pkgs.gh
+            pkgs.tailscale
+            pkgs.wayvnc
+            pkgs.zapret
 
-          # Input Method & Addons
-          (pkgs.kdePackages.fcitx5-with-addons.override {
-            addons = [
-              pkgs.kdePackages.fcitx5-unikey
-              pkgs.kdePackages.fcitx5-configtool
-              pkgs.fcitx5-gtk
-            ];
-          })
+            # Input Method & Addons
+            fcitx5WithAddons
 
-          # Fonts
-          pkgs.nerd-fonts.monaspace
+            # Fonts
+            pkgs.nerd-fonts.monaspace
 
-          # GPU wrapper
-          nixGLIntel
-        ];
+            # GPU wrapper
+            nixGLIntel
+          ];
+        };
+
+        fcitx5 = fcitx5WithAddons;
+        fcitx5-with-addons = fcitx5WithAddons;
       };
     };
 }
